257257119df038ee833de2316489c2f3fe59ee78
Yibo_Cheng <cyb66765556@gmail.com>
2020-05-30 21:40:12 -0500

    Project Initialization

---
+++ drchain/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome to Dr.Chain</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
