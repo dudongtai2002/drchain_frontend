81387da91dbc762fafc9f368ceb104128d3d6934
regulus17 <zhangfan8787@gmail.com>
2020-06-15 02:00:02 +0800

    update account page header style

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Card, Divider } from 'antd';
 import './index.less'
 
 export default function PageHeader({title}) {
     return (
-        <Card className="page-header-card" bordered={false} bodyStyle={{padding: "24px 0"}}>
+        <div className="page-header-container">
             <div className="page-header">
                 <span className="header-title">{title}</span>
             </div>
-        </Card>
+        </div>
         
     )
 }
