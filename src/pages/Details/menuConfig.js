8ea82e633f14c08d7cd90004901d9acca7a1d601
Yibo_Cheng <cyb66765556@gmail.com>
2020-06-14 21:11:26 -0500

    Add selected department function in the detail page

---
+++ src/pages/Details/menuConfig.js
@@ -0,0 +1,67 @@
+const menuList = [
+    {
+        title: 'Primary Care(PCP)',
+        children:[
+            {
+                title:"General Practice", 
+            },
+            {title:"Pediatric"},
+        ]
+    },
+    {
+        title: "Specialists",
+        children:[
+            {title:"Acupuncturology"},
+            {title:"Audiology"},
+            {title:"Cardiology"},
+            {title:"Chiropractic"},
+            {title:"Dermatology"},
+            {title:"Endocrinology"},
+            {title:"Gastroenterology"},
+            {title:"General Surgical"},
+            {title:"Internl Medicine"},
+            {title:"Neurology"},
+            {title:"Neurosurgical"},
+            {title:"Occupational Therapy"},
+            {title:"Oncology"},
+            {title:"Ophthalmology"},
+            {title:"Optometry"},
+            {title:"Oral Surgical"},
+            {title:"Orthopediatric"},
+            {title:"Otolaryngology(ENT)"},
+            {title:"Pediatric Internal Medicine"},
+            {title:"Physical Therapy"},
+            {title:"Podiatrology"},
+            {title:"psychopathology"},
+            {title:"Pulmonology"},
+            {title:"Radiology"},
+            {title:"Rheumatology"},
+            {title:"Urology"},
+        ]
+    },
+    {
+        title: 'OB-GYN',
+        children:[
+            {
+                title:"OB-GYN", 
+            }
+        ]
+    },
+    {
+        title: 'Dental',
+        children:[
+            {
+                title:"Dental", 
+            }
+        ]
+    },
+    {
+        title: 'Vision',
+        children:[
+            {
+                title:"Vision", 
+            }
+        ]
+    },
+];
+export default menuList;
