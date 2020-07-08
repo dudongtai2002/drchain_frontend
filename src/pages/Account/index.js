9027ea60577c1fd9d6be3c366a1c6ee71d6ba50a
Dongtai Du <dudongtai2002@gmail.com>
2020-07-08 00:42:21 -0400

    changed file permissions, add icons to account page header

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.less'
-import {Card, Row, Col} from 'antd'
+import {Card, Row, Col, Icon} from 'antd'
 import AccountInfo from './accountInfo'
 import AccountSettings from './accountSettings'
 import PageHeader from '../../components/PageHeader'
@@ -10,7 +10,7 @@ export default class Account extends React.Component {
     render(){
         return (
         <div className="account-container">
-            <PageHeader title="Account Settings" />
+            <PageHeader title="Account Settings" icons={[<Icon type="reload" />, <Icon type="more" />]}/>
             <Card className="card-wrap" >
                 <Row>
                     <Col xs={24} sm={24} md={24} lg={12} xl={14} className="section-container">
